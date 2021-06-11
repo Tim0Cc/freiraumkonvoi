7460bcbd2672184f7cfa5864b7490d492ff0c2de
Tim Cecatka <tim.cecatka@gmail.com>
2021-06-11 22:44:22 +0200

    Post model

--- models/post.js
+++ models/post.js
@@ -1,1 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const Post = mongoose.model('Post', PostSchema)
+
+module.exports = Post
