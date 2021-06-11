2c5b8dc6d569833775830a18d8ebc2a6f39b4e7c
Tim Cecatka <tim.cecatka@gmail.com>
2021-06-12 00:44:06 +0200

    posts mvc READ & CREATE

--- routes/posts.js
+++ routes/posts.js
@@ -3,8 +3,42 @@ router = express.Router()
 const Post = require('../models/post')
 const User = require('../models/user')
 
-router.get('/', (req, res) => {
-  res.send('Post')
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find({})
+    const users = await User.find({})
+    res.render('posts/index', {
+      posts, users
+    })
+  } catch (error) {
+    res.redirect('/')
+  }
+})
+
+router.get('/new', async (req, res) => {
+  try {
+    const users = await User.find({})
+    const post = new Post()
+    res.render('posts/new', { users, post })
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+router.post('/', async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+    user: req.body.user
+  })
+  console.log(post)
+  try {
+    await post.save()
+    res.redirect('posts')
+  } catch (error) {
+    console.log(error)
+    res.redirect('posts/new')
+  }
 })
 
 module.exports = router
