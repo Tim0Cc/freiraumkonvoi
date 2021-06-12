ad25697f69b2f35a27fd03bed01859971734582d
Tim Cecatka <tim.cecatka@gmail.com>
2021-06-12 23:32:14 +0200

    users READ show

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const Post = require('../models/post')
 const { ensureAuthenticated, authRole } = require('../config/auth')
 
 router.get('/', ensureAuthenticated, async (req, res) => {
   try {
     const users = await User.find({})
     res.render('users/index', { users })
+  } catch (error) {
+    console.error(error)
+    res.redirect('/')
+  }
+})
+
+router.get('/:id', ensureAuthenticated, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    const posts = await Post.find({}).where('user').equals(`${user.id}`).exec()
+    res.render('users/show', { user, posts })
   } catch (error) {
     console.error(error)
     res.redirect('/')
