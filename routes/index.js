824a308ae4c8f56608087ef268e84619379923ab
Tim Cecatka <tim.cecatka@gmail.com>
2021-06-12 12:58:57 +0200

    dynamic Navbar Logout / Login&Registrieren

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
 const { ensureAuthenticated, authRole } = require('../config/auth') 
 
 // login page
--- server.js
+++ server.js
@@ -39,6 +39,10 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  next()
+})
+app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
