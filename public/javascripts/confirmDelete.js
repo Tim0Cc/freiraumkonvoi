6b853550c6955a63ff7c9be2f7236c075262d465
Tim Cecatka <tim.cecatka@gmail.com>
2021-06-17 15:33:11 +0200

    BUGFIX: deleteFormsas Array.forEach

--- public/javascripts/confirmDelete.js
+++ public/javascripts/confirmDelete.js
@@ -1,10 +1,13 @@
-const deleteForm = document.getElementById("deleteForm");
-if (deleteForm =! null) {
-  deleteForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let result = confirm("Möchte Sie wirklich löschen?");
-    if (result) {
-      deleteForm.submit();
-    }
+const deleteForms = document.getElementsByClassName("deleteForm");
+if (deleteForms.length > 0) {
+  console.log('in if statement')
+  Array.from(deleteForms).forEach((deleteForm) => {
+    deleteForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      let result = confirm("Möchte Sie wirklich löschen?");
+      if (result) {
+        deleteForm.submit();
+      }
+    })
   })
 }
