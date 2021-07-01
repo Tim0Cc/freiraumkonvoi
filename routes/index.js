9f35fcc6c14a22d40e8f4667c16741c690a88acb
Tim Cecatka <tim.cecatka@gmail.com>
2021-07-01 15:14:17 +0200

    tools page

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
+router.get('/tools', ensureAuthenticated, (req, res) => {
+  res.render('../public/pages/tools')
+})
+
 //dashboard page
 router.get('/dashboard', ensureAuthenticated, (req,res) => {
   res.render('users/dashboard', {
